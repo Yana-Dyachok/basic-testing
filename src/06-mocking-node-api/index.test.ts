8ea3f2ddc5b914847add1a3a478828c8e2f4ca25
Yana <annetart1994@gmail.com>
2024-10-20 10:49:17 +0300

    feat: add tests for 07-mocking-lib-api

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -5,32 +5,32 @@ import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
-    jest.useFakeTimers(); 
+    jest.useFakeTimers();
   });
 
   afterAll(() => {
-    jest.useRealTimers(); 
+    jest.useRealTimers();
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    const setTimeoutSpy = jest.spyOn(global, 'setTimeout'); 
-    const callback = jest.fn(); 
+    const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+    const callback = jest.fn();
 
-    doStuffByTimeout(callback, 1); 
+    doStuffByTimeout(callback, 1);
 
-    expect(setTimeoutSpy).toHaveBeenCalledTimes(1); 
-    expect(setTimeoutSpy).toHaveBeenLastCalledWith(callback, 1); 
+    expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+    expect(setTimeoutSpy).toHaveBeenLastCalledWith(callback, 1);
   });
 
   test('should call callback only after timeout', () => {
     const callback = jest.fn();
 
     doStuffByTimeout(callback, 1000);
-    expect(callback).not.toBeCalled(); 
+    expect(callback).not.toBeCalled();
 
-    jest.runAllTimers(); 
+    jest.runAllTimers();
 
-    expect(callback).toBeCalled(); 
+    expect(callback).toBeCalled();
     expect(callback).toHaveBeenCalledTimes(1);
   });
 });
@@ -45,55 +45,57 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    const setIntervalSpy = jest.spyOn(global, 'setInterval'); 
+    const setIntervalSpy = jest.spyOn(global, 'setInterval');
     const callback = jest.fn();
 
     doStuffByInterval(callback, 1);
 
-    expect(setIntervalSpy).toHaveBeenCalledTimes(1); 
-    expect(setIntervalSpy).toHaveBeenLastCalledWith(callback, 1); 
+    expect(setIntervalSpy).toHaveBeenCalledTimes(1);
+    expect(setIntervalSpy).toHaveBeenLastCalledWith(callback, 1);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
     const callback = jest.fn();
 
     doStuffByInterval(callback, 1000);
-    expect(callback).not.toBeCalled(); 
+    expect(callback).not.toBeCalled();
 
-    jest.advanceTimersByTime(1000); 
+    jest.advanceTimersByTime(1000);
 
-    expect(callback).toBeCalled(); 
-    expect(callback).toHaveBeenCalledTimes(1); 
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
 
-    jest.advanceTimersByTime(1000); 
-    expect(callback).toHaveBeenCalledTimes(2); 
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    const joinSpy = jest.spyOn(path, 'join'); 
+    const joinSpy = jest.spyOn(path, 'join');
 
     await readFileAsynchronously('test.txt');
 
-    expect(joinSpy).toHaveBeenCalledWith(expect.anything(), 'test.txt'); 
+    expect(joinSpy).toHaveBeenCalledWith(expect.anything(), 'test.txt');
   });
 
   test('should return null if file does not exist', async () => {
-    jest.spyOn(fs, 'existsSync').mockReturnValueOnce(false); 
+    jest.spyOn(fs, 'existsSync').mockReturnValueOnce(false);
 
     const result = await readFileAsynchronously('test.txt');
 
-    expect(result).toBeNull(); 
+    expect(result).toBeNull();
   });
 
   test('should return file content if file exists', async () => {
     const fileContents = 'Check';
-    jest.spyOn(fs, 'existsSync').mockReturnValueOnce(true); 
-    jest.spyOn(fsPromises, 'readFile').mockResolvedValue(Buffer.from(fileContents)); 
+    jest.spyOn(fs, 'existsSync').mockReturnValueOnce(true);
+    jest
+      .spyOn(fsPromises, 'readFile')
+      .mockResolvedValue(Buffer.from(fileContents));
 
     const result = await readFileAsynchronously('test.txt');
 
-    expect(result).toBe(fileContents); 
+    expect(result).toBe(fileContents);
   });
 });
